a272ea95c49a01898a31f287205573a8553aecd1
Kranks <ltvorko@gmail.com>
2018-12-17 18:33:00 -0500

    Buff spell type

--- 4SS-ProjectPrototype/Assets/Scripts/Sorts script/BuffSpell.cs
+++ 4SS-ProjectPrototype/Assets/Scripts/Sorts script/BuffSpell.cs
@@ -11,30 +11,46 @@ public class BuffSpell : Spell {
     public override void launch(Vector3 position) {
         Collider2D[] targets = Physics2D.OverlapCircleAll(position, portee);
         foreach (var target in targets) {
-            Aspect aspect = target.GetComponent<Aspect>();
-            if (aspect.aspectType == targetType) {
-                if (target.GetComponent<EnemyController2D>())
-                    Buff(target.GetComponent<EnemyController2D>());
-                if (target.GetComponent<PlayerController>()) {
-                    Buff(target.GetComponent<PlayerController>());
+            if (!target.isTrigger) {
+                Aspect aspect = target.GetComponent<Aspect>();
+                if (aspect) {
+                    if (aspect.aspectType == targetType) {
+                        if (target.GetComponent<EnemyController2D>()) {
+                            Buff(target.GetComponent<EnemyController2D>());
+                        }
+                        if (target.GetComponent<PlayerController>()) {
+                            Buff(target.GetComponent<PlayerController>());
+                        }
+                    }
                 }
             }
         }
     }
 
     public override void launch(Vector3 position, Transform target) {
-        throw new System.NotImplementedException();
+        launch(position);
     }
 
-    IEnumerator Buff(EnemyController2D enemy) {
+
+    void Buff(EnemyController2D enemy) {
+        enemy.StartCoroutine(EnemyBuff(enemy));
+    }
+
+    void Buff(PlayerController player) {
+        player.StartCoroutine(PlayerBuff(player));
+    }
+
+    IEnumerator EnemyBuff(EnemyController2D enemy) {
+        Stats currentStats = Instantiate(enemy.infos.stats);
         enemy.infos.stats.Add(stats);
         yield return new WaitForSeconds(duree);
-        enemy.infos.stats.Remove(stats);
+        enemy.infos.stats.SetStats(currentStats);
     }
 
-    IEnumerator Buff(PlayerController player) {
+    IEnumerator PlayerBuff(PlayerController player) {
+        Stats currentStats = Instantiate(player.player.stats);
         player.player.stats.Add(stats);
         yield return new WaitForSeconds(duree);
-        player.player.stats.Remove(stats);
+        player.player.stats.SetStats(currentStats);
     }
 }
